88b063910735e26349f44e741825e08c1397fb88
outoflu <jiangdongxian@outlook.com>
2024-05-07 16:13:01 +0800

    continue

--- TestRedisConnect.h
+++ TestRedisConnect.h
@@ -1,5 +1,4 @@
-#ifndef __TEST__REDIS__
-#define __TEST__REDIS__
+#pragma once
 #include "hiredis.h"
 #include <iostream>
 
@@ -8,7 +7,7 @@
 * 使用GBK会导致密码内容正确但不会被正确识别导致连不上，我也不知道为什么
 */
 using namespace std;
-
+namespace TestRedisConnect {
 inline void TestRedis() {
 	// redis默认监听端口为6379 可以再配置文件中修改
 	// 连接redis
@@ -103,4 +102,26 @@ inline void TestRedis() {
 
 }
 
-#endif
+inline void TestRedisMgr() {
+	assert(RedisMgr::GetInstance()->Connect("127.0.0.1", 6380));
+	assert(RedisMgr::GetInstance()->Auth("123456"));
+	assert(RedisMgr::GetInstance()->Set("blogwebsite","xxx.club"));
+	std::string value="";
+	assert(RedisMgr::GetInstance()->Get("blogwebsite", value) );
+	assert(RedisMgr::GetInstance()->Get("nonekey", value) == false);
+	assert(RedisMgr::GetInstance()->HSet("bloginfo","blogwebsite", "xxx.club"));
+	assert(RedisMgr::GetInstance()->HGet("bloginfo","blogwebsite") != "");
+	assert(RedisMgr::GetInstance()->ExistsKey("bloginfo"));
+	assert(RedisMgr::GetInstance()->Del("bloginfo"));
+	assert(RedisMgr::GetInstance()->Del("bloginfo"));
+	assert(RedisMgr::GetInstance()->ExistsKey("bloginfo") == false);
+	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue1"));
+	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue2"));
+	assert(RedisMgr::GetInstance()->LPush("lpushkey1", "lpushvalue3"));
+	assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+	assert(RedisMgr::GetInstance()->RPop("lpushkey1", value));
+	assert(RedisMgr::GetInstance()->LPop("lpushkey1", value));
+	assert(RedisMgr::GetInstance()->LPop("lpushkey2", value)==false);
+	RedisMgr::GetInstance()->Close();
+}
+}
